7b2b167708dfccd33679828fa6b228dad0b620a8
Harold Harting <94148624+hharting14@users.noreply.github.com>
2022-05-03 18:26:30 -0400

    Update settings.py

--- thatheavydiscord/settings.py
+++ thatheavydiscord/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'tm&#$hc16wh_x()ghr$ei1mh*ng^njmvf5=%_z2%@@e8t@ysf$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
